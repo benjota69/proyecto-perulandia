719432ebfff70117e828b8e3d17a57f1ef4ca3b1
Basknight1 <basknight01@gmail.com>
2025-05-19 15:03:12 -0400

    Add files via upload

---
+++ ProyectoPerulandiaC/src/main/java/com/example/ProyectoPerulandiaC/ProyectoPerulandiaCApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoPerulandiaC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoPerulandiaCApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoPerulandiaCApplication.class, args);
+	}
+
+}
---
+++ ProyectoPerulandiaC/src/test/java/com/example/ProyectoPerulandiaC/ProyectoPerulandiaCApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProyectoPerulandiaC;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoPerulandiaCApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
